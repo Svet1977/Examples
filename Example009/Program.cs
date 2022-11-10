9fd73dd0e3d551ae37b3a510d80bbb1f174ab8f2
Svetlana <Mse1977@bk.ru>
2022-11-10 22:45:55 +0300

    Мax из массива 9 чисел

---
+++ Example009/Program.cs
@@ -0,0 +1,24 @@
+﻿// использование массива при написании кода
+//max из массива из 9 чисел
+
+
+int Max(int ard1, int ard2,int ard3)
+{
+    int result = ard1;
+//result - внутренний результат, в нем будет храниться наш max
+    if(ard2>result) result=ard2;
+    if(ard3>result) result=ard3;
+    return result;
+    // return  result - возвращение методом результата
+}
+
+//вводим массив
+int [] array={11, 21, 31, 41, 15, 61, 17, 18, 19};
+
+//вычисляем max элемент массива, вызывая функцию Max в каждой тройке
+int max=Max(Max(array[0],array[1],array[2]),
+            Max(array[3],array[4],array[5]),  
+            Max(array[6],array[7],array[8]));     
+
+//выводим результат
+Console.WriteLine(max);
